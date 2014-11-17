d4abd3a10922ff2d5b858480fd10e681a04281f3
EmiPatoR <emipator@gmail.com>
2014-11-17 23:15:31 +0100

    Perceptron

--- includes/hmm.h
+++ includes/hmm.h
@@ -27,9 +27,11 @@ typedef struct
 
 
 hmm* allocate_hmm(int nbe, int nbo);
+
 void intialize_hmm(hmm *h);
 void print_hmm(hmm *h, char *file_name);
 void print_hmm_verbose(hmm *h, char *file_name);
+
 hmm* load_hmm(char *file_name);
 int* charge_observables(char *file_name, int T);
 void free_hmm(hmm *h);
--- src/main.c
+++ src/main.c
@@ -3,18 +3,40 @@
 
 #include "mldata.h"
 #include "traintestsplit.h"
+#include "structures.h"
 
 int main()
 {
+	int nbMots = 0;
+	int nbPhrases = 0;
+
 	TrainTestSplit* tts = allocate_traintestsplit();
 	load_count(tts,50,0);
 
 	MlData* data = load_from_file("data/vocab",*tts);
 
-    printf("Hello world!\n");
+	mot* Mots = NULL;
+	Mots = init_mots("data/vocab",&nbMots);
 
+	phrase* p = NULL;
+	p = init_phrases("data/dev/ftb.dev.encode",&nbPhrases);
+
+    printf("La taille du fichier d'entrainement est : %i \n",tts->train_sample_part.count);
+    printf("La taille du fichier de test est : %i \n",data->samples_count - tts->train_sample_part.count);
+
+
+    printf("Les mots ont ete correctements charges en memoire. \n");
+    printf("Il y a %i mots . \n",nbMots);
+    printf("Mot 11502 = %s / id : %i / longueur : %i \n",Mots[18234].caractere,Mots[18234].id,Mots[18234].longueur);
+
+    printf("Il y a %i phrases .\n",nbPhrases);
+
+
+    free_mots(Mots,nbMots);
+    printf("Mots liberes en memoire.\n");
     free_traintestsplit(tts);
     free_data(data);
+    printf("Echantillons liberes en memoire.\n");
 
     return 0;
 }
